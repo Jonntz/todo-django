d9d2ab76c35f3f77f4572c4c74dc7cbf61a3cacd
Jonntz <monteirosousa23@gmail.com>
2020-09-19 01:59:52 -0300

    Adicionando a estrutura do projeto

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+from .views import *
+from django.urls import path
+
+urlpatterns = [
+    path('helloWorld', hello_world),
+    path('', task_list, name='task_list'),
+    path('yourname/<str:name>', your_name, name="your_name")
+]
