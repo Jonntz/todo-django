67c1d23e63758321be76e3b61a8950a83f6ed5d0
Jonntz <monteirosousa23@gmail.com>
2020-09-21 18:12:46 -0300

    CRUD de Criação de tarefa

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,8 +2,7 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('helloWorld', hello_world),
     path('', task_list, name='task_list'),
-    path('yourname/<str:name>', your_name, name="your_name"),
+    path('newtask/', new_task, name='new_task'),
     path('task/<int:id>', task_view, name='task_view'),
 ]
