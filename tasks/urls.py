76c4f1d0096b98ae361de2f3a3e5723e3b60a44e
Jonntz <monteirosousa23@gmail.com>
2020-09-21 17:43:13 -0300

    Iniciando a CRUD com a leitura de todas as tarefas e os detalhes

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('helloWorld', hello_world),
     path('', task_list, name='task_list'),
-    path('yourname/<str:name>', your_name, name="your_name")
+    path('yourname/<str:name>', your_name, name="your_name"),
+    path('task/<int:id>', task_view, name='task_view'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse    
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
 def hello_world(request):
     return HttpResponse("Hello world")
 
-def task_list(request):
-    return render(request, 'tasks/list.html')
-
 def your_name(request, name):
     return render(request, 'tasks/your-name.html', {'name': name})
+
+def task_view(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
